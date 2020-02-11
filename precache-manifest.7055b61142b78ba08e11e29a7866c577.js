9e0a068da4625c4572e61af8514db02d9e562e47
ralphwright <ralphwrightengineer@gmail.com>
2020-02-10 21:46:55 -0600

    Updates

---
+++ precache-manifest.7055b61142b78ba08e11e29a7866c577.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61d447e063f1ae776a657006140dab3d",
+    "url": "/friend-finder/index.html"
+  },
+  {
+    "revision": "36102aa383974fa8c353",
+    "url": "/friend-finder/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "463a0cd15a70826e16f2",
+    "url": "/friend-finder/static/css/main.eab410dd.chunk.css"
+  },
+  {
+    "revision": "36102aa383974fa8c353",
+    "url": "/friend-finder/static/js/2.acc506de.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/friend-finder/static/js/2.acc506de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "463a0cd15a70826e16f2",
+    "url": "/friend-finder/static/js/main.6d1a6b06.chunk.js"
+  },
+  {
+    "revision": "a99e38ca494a9b23dbc2",
+    "url": "/friend-finder/static/js/runtime-main.242d5029.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/friend-finder/static/media/SEGA.12a3db8b.woff"
+  }
+]);
